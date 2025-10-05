eb61e466d017c7c48a937e36149751917676b4b6
bearxiao9756 <bearxiao9@gmail.com>
2025-10-05 18:27:18 +0800

    更换IP

--- public/tsdd-config.js
+++ public/tsdd-config.js
@@ -1,7 +1,8 @@
 var TSDD_CONFIG = {
   // APP_URL: 'https://43.160.244.68/v1/'
   // APP_URL: 'http://43.160.244.68/v1'
-  APP_URL: 'http://43.160.244.68/v1'
+  // APP_URL: 'http://43.160.244.68/v1'
+  APP_URL:'https://hy82s2hjk23.icu/v1'
 };
 
 window.TSDD_CONFIG = TSDD_CONFIG;
