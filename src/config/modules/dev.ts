f1ddc195794a24c9709129dd3cc8f2c2e0baaed6
bearxiao9756 <bearxiao9@gmail.com>
2025-10-31 12:55:37 +0800

    com

--- src/config/modules/dev.ts
+++ src/config/modules/dev.ts
@@ -2,6 +2,7 @@ export default {
   APP_ENV: 'dev',
   // APP_URL: 'https://43.160.244.68/v1/'
   // APP_URL: 'https://43.160.244.68/v1/'
-  // APP_URL: 'http://43.160.244.68/v1'
-  APP_URL:'https://hy82s2hjk23.icu/v1'
+  APP_URL: 'http://43.134.241.145/v1'
+  // APP_URL:'https://hy82s2hjk23.icu/v1'
+  
 };
