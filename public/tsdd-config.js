8820e23f6e1ced7847551ff5ab3539bce4ef73e3
bearxiao9756 <bearxiao9@gmail.com>
2025-11-10 21:24:43 +0800

    更换域名

--- public/tsdd-config.js
+++ public/tsdd-config.js
@@ -2,7 +2,7 @@ var TSDD_CONFIG = {
   // APP_URL: 'https://43.160.244.68/v1/'
   // APP_URL: 'http://43.160.244.68/v1'
   // APP_URL: 'http://43.134.241.145/v1'
-  APP_URL:'https://bao.abhao.icu/v1'
+  APP_URL:'https://bao.aehao.icu/v1'
   
 };
 
--- src/config/modules/dev.ts
+++ src/config/modules/dev.ts
@@ -3,6 +3,6 @@ export default {
   // APP_URL: 'https://43.160.244.68/v1/'
   // APP_URL: 'https://43.160.244.68/v1/'
   // APP_URL: 'http://43.134.241.145/v1'
-  APP_URL:'https://bao.abhao.icu/v1'
+  APP_URL:'https://bao.aehao.icu/v1'
   
 };
