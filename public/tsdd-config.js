e3019fd862782a87652b2abf0fef938c7304e160
bearxiao9756 <bearxiao9@gmail.com>
2025-11-13 00:20:36 +0800

    更新域名

--- public/tsdd-config.js
+++ public/tsdd-config.js
@@ -2,7 +2,7 @@ var TSDD_CONFIG = {
   // APP_URL: 'https://43.160.244.68/v1/'
   // APP_URL: 'http://43.160.244.68/v1'
   // APP_URL: 'http://43.134.241.145/v1'
-  APP_URL:'https://b.imhao.icu/v1'
+  APP_URL:'https://c.imhao.icu/v1'
   
 };
 
