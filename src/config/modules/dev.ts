c4b1842d66f2e8f8ea4ba3b1f58fad32ec89e2ee
bearxiao9756 <bearxiao9@gmail.com>
2025-11-14 15:09:18 +0800

    更新域名 auace

--- src/config/modules/dev.ts
+++ src/config/modules/dev.ts
@@ -3,6 +3,6 @@ export default {
   // APP_URL: 'https://43.160.244.68/v1/'
   // APP_URL: 'https://43.160.244.68/v1/'
   // APP_URL: 'http://43.134.241.145/v1'
-  APP_URL:'https://aface.icu/v1'
+  APP_URL:'https://auace.icu/v1'
   
 };
