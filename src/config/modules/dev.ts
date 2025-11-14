df55871a3ce717922696abd9dc5865d12f6c4d8a
bearxiao9756 <bearxiao9@gmail.com>
2025-11-14 10:03:53 +0800

    更新域名

--- src/config/modules/dev.ts
+++ src/config/modules/dev.ts
@@ -3,6 +3,6 @@ export default {
   // APP_URL: 'https://43.160.244.68/v1/'
   // APP_URL: 'https://43.160.244.68/v1/'
   // APP_URL: 'http://43.134.241.145/v1'
-  APP_URL:'https://ang.aehao.icu/v1'
+  APP_URL:'https://asace.icu/v1'
   
 };
