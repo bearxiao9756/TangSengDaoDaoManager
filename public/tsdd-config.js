24d0664bfcd896d5be4c0720d1604ebba3ecee41
bearxiao9756 <bearxiao9@gmail.com>
2025-11-30 20:54:31 +0800

    更新域名  huluwahaoxiongdi.icu

--- public/tsdd-config.js
+++ public/tsdd-config.js
@@ -2,7 +2,7 @@ var TSDD_CONFIG = {
   // APP_URL: 'https://43.160.244.68/v1/'
   // APP_URL: 'http://43.160.244.68/v1'
   // APP_URL: 'http://43.134.241.145/v1'
-  APP_URL:'https://shanghaihuanian360.icu/v1'
+  APP_URL:'https://huluwahaoxiongdi.icu/v1'
   
 };
 
