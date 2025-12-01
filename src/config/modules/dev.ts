7e5ef70191a2c1131501c09a0ab17276788b14e0
bearxiao9756 <bearxiao9@gmail.com>
2025-12-01 12:36:02 +0800

    更新域名

--- src/config/modules/dev.ts
+++ src/config/modules/dev.ts
@@ -3,6 +3,6 @@ export default {
   // APP_URL: 'https://43.160.244.68/v1/'
   // APP_URL: 'https://43.160.244.68/v1/'
   // APP_URL: 'http://43.134.241.145/v1'
-  APP_URL:'https://huluwahaoxiongdi.icu/v1'
+  APP_URL:'https://chuqingqing.icu/v1'
   
 };
