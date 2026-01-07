e6be24aea61234391a3ed0e632f635c0f73f0ad4
bearxiao9756 <bearxiao9@gmail.com>
2026-01-07 22:10:34 +0800

    换皮
    九威清算

--- src/layouts/components/Footer.vue
+++ src/layouts/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer flex-center">
-    <a href="https://www.baidu.com" target="_blank"> 2023 © 汇同清算. </a>
+    <a href="https://www.baidu.com" target="_blank"> 2023 © 九威清算. </a>
   </div>
 </template>
 
